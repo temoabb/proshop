8e72aa6b6b61d46471ea72f17e13bb237dd2a6b7
temoabb <abesadzetemo1997@gmail.com>
2021-10-12 16:34:26 +0400

    Initial commit, frontend and webpack setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      This is an App component
+    </div>
+  )
+};
+
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path"); // absolute path to the output;
+const HtmlPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+
+    // This will create an absolute path to 'build'; We do not have 'build', but it will create 'build' folder for us; 
+    // By default it is 'dist', but we want to name it 'build';
+  },
+  module: {
+    rules: [ // rules are how we are gonna handle certain file types
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader', // This package (babel-loader) allows transpiling JavaScript files using Babel and webpack.
+          }
+        ]
+
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      filename: 'index.html',
+      template: './public/index.html'
+    })
+  ],
+
+  devServer: {
+    historyApiFallback: true, // this have to be true, if you ever running into the issues like react-router-dom
+    port: 5000,
+    proxy: {
+      '/api': 'http://localhost:7070'
+    },
+  }
+}
